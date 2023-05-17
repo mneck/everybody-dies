898dc7064eb1e434c4e4d30e5454a2f2031a5e68
Matt N <mneckelmann@gmail.com>
2023-05-17 22:30:54 +0200

    Adds Will and POA functions instead of components

--- src/pages/ReviewDocumentPage.jsx
+++ src/pages/ReviewDocumentPage.jsx
@@ -85,6 +85,140 @@ function ReviewDocumentPage(props) {
     navigator.clipboard.writeText(content);
   }
 
+  function returnPoa() {
+    return (
+      <div id="content" className="review-field">
+        <h3>
+          <pre>Power of Attorney</pre>
+        </h3>
+        <pre>
+          <p>
+            This GENERAL POWER OF ATTORNEY is given on{" "}
+            {new Date().toLocaleDateString("en-US", {
+              month: "long",
+              day: "numeric",
+              year: "numeric",
+            })}{" "}
+            by{" "}
+            {/*
+          function getAnswerByCueId(arr, cueId) {arr.find()}
+        
+        */}
+            {getAnswerByCueId(answers, CueIds.YourName)}: of{" "}
+            {getAnswerByCueId(answers, CueIds.YourAddress)}.
+          </p>
+        </pre>
+
+        <pre>
+          <p>I appoint the following person:</p>
+        </pre>
+
+        <pre>
+          <p>
+            {"      "}
+            ##Name of Attorney, of ##Address, Vancouver
+          </p>
+        </pre>
+
+        <pre>
+          <p>
+            to be my attorney in accordance with the{" "}
+            <i>Power of Attorney Act</i> and to do on my
+            behalf anything that I can lawfully do by an
+            attorney.
+          </p>
+        </pre>
+
+        <pre>
+          <p>
+            Power of Attorney may be exercised during any
+            subsequent mental infirmity on my part.
+          </p>
+        </pre>
+
+        <pre>
+          <p>
+            I revoke all other powers of attorney previously
+            granted by me.
+          </p>
+        </pre>
+
+        <pre>
+          <p>
+            <b>Executions(s)</b>
+          </p>
+        </pre>
+      </div>
+    );
+  }
+
+  function returnWill() {
+    return (
+      <div id="content" className="review-field">
+        <h3>
+          <pre>Will</pre>
+        </h3>
+        <pre>
+          <p>
+            This GENERAL POWER OF ATTORNEY is given on{" "}
+            {new Date().toLocaleDateString("en-US", {
+              month: "long",
+              day: "numeric",
+              year: "numeric",
+            })}{" "}
+            by{" "}
+            {/*
+            function getAnswerByCueId(arr, cueId) {arr.find()}
+          
+          */}
+            {getAnswerByCueId(answers, CueIds.YourName)}: of{" "}
+            {getAnswerByCueId(answers, CueIds.YourAddress)}.
+          </p>
+        </pre>
+
+        <pre>
+          <p>I appoint the following person:</p>
+        </pre>
+
+        <pre>
+          <p>
+            {"      "}
+            ##Name of Attorney, of ##Address, Vancouver
+          </p>
+        </pre>
+
+        <pre>
+          <p>
+            to be my attorney in accordance with the{" "}
+            <i>Power of Attorney Act</i> and to do on my
+            behalf anything that I can lawfully do by an
+            attorney.
+          </p>
+        </pre>
+
+        <pre>
+          <p>
+            Power of Attorney may be exercised during any
+            subsequent mental infirmity on my part.
+          </p>
+        </pre>
+
+        <pre>
+          <p>
+            I revoke all other powers of attorney previously
+            granted by me.
+          </p>
+        </pre>
+
+        <pre>
+          <p>
+            <b>Executions(s)</b>
+          </p>
+        </pre>
+      </div>
+    );
+  }
+
   // let cues;
   // documentData.DocumentType === "poa"
   //   ? (cues = poaCues)
@@ -149,7 +283,9 @@ function ReviewDocumentPage(props) {
               Copy text to clipboard
             </button>
 
-            <PoaForm props={answers} />
+            {documentData.documentType === "poa"
+              ? returnPoa()
+              : returnWill()}
 
             <button onClick={deleteDocument}>
               Delete Document
